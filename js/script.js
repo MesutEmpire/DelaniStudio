4a613424bfbb61f445d73e371f0d2296529036d4
Samuel Wainaina <samwainaina01@gmail.com>
2021-06-20 17:54:40 +0300

    Add fadeToggle

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#design-img").click(function () {
+    $("#design-info").fadeToggle("slow");
+  });
+  $("#development-img").click(function () {
+    $("#development-info").fadeToggle("slow");
+  });
+  $("#product-img").click(function () {
+    $("#product-info").fadeToggle("slow");
+  });
+});
