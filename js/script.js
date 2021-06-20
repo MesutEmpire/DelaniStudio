8a33748f9d1722296652b3c5be3299d46990f66e
Samuel Wainaina <samwainaina01@gmail.com>
2021-06-20 21:17:52 +0300

    Add show function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
   $("#design-img").click(function () {
     $("#design-info").fadeToggle("slow");
+    $("#design-img").hide();
   });
   $("#development-img").click(function () {
     $("#development-info").fadeToggle("slow");
+    $("#development-img").hide();
   });
   $("#product-img").click(function () {
     $("#product-info").fadeToggle("slow");
+    $("#product-img").hide();
+  });
+  $("#design-info").click(function () {
+    $("#design-img").show();
+    $("#design-info").hide();
+  });
+  $("#development-info").click(function () {
+    $("#development-img").show();
+    $("#development-info").hide();
+  });
+  $("#product-info").click(function () {
+    $("#product-img").show();
+    $("#product-info").hide();
   });
 });
